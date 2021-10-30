3b217f84e51f349387c04b193c9e41ac0c0d0a55
Elise_LaBonte <emlabonte714@gmail.com>
2021-10-30 13:30:01 -0400

    config for heroku

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,18 @@ const environment = process.env;
 
 let sequelize;
 
-if (environment.JAWSDB_URL) {
-  sequelize = new Sequelize(environment.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    environment.DB_NAME,
-    environment.DB_USER,
-    environment.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+    sequelize = new Sequelize(
+        environment.DB_NAME,
+        environment.DB_USER,
+        environment.DB_PASSWORD, {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
 }
 
 module.exports = sequelize;
